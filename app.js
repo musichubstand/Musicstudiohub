9da0c8fabcabe2fcf9395b2097e377799cb9c3c0
musichubstand <134685992+musichubstand@users.noreply.github.com>
2023-05-26 14:02:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,79 +7,79 @@ const songs = [
         id:'1',
         songName:` On My Way <br>
         <div class="subtitle">Alan Walker</div>`,
-        poster: "img/1.jpg"
+        poster: "1.jpg"
     },
     {
         id:'2',
         songName:` Alan Walker-Fade <br>
         <div class="subtitle">Alan Walker</div>`,
-        poster: "img/2.jpg"
+        poster: "2.jpg"
     },
     // all object type 
     {
         id:"3",
         songName: `Cartoon - On & On <br><div class="subtitle"> Daniel Levi</div>`,
-        poster: "img/3.jpg",
+        poster: "3.jpg",
     },
     {
         id:"4",
         songName: `Warriyo - Mortals <br><div class="subtitle">Mortals</div>`,
-        poster: "img/4.jpg",
+        poster: "4.jpg",
     },
     {
         id:"5",
         songName: `Ertugrul Gazi <br><div class="subtitle">Ertugrul</div>`,
-        poster: "img/5.jpg",
+        poster: "5.jpg",
     },
     {
         id:"6",
         songName: `Electronic Music <br><div class="subtitle">Electro</div>`,
-        poster: "img/6.jpg",
+        poster: "6.jpg",
     },
     {
         id:"7",
         songName: `Agar Tum Sath Ho <br><div class="subtitle">Tamashaa</div>`,
-        poster: "img/7.jpg",
+        poster: "7.jpg",
     },
     {
         id:"8",
         songName: `Suna Hai <br><div class="subtitle">Neha Kakker</div>`,
-        poster: "img/8.jpg",
+        poster: "8.jpg",
     },
     {
         id:"9",
         songName: `Dilber <br><div class="subtitle">Satyameva Jayate</div>`,
-        poster: "img/9.jpg",
+        poster: "/9.jpg",
     },
     {
         id:"10",
         songName: `Duniya <br><div class="subtitle">Luka Chuppi</div>`,
-        poster: "img/10.jpg",
+        poster: "10.jpg",
     },
     {
         id:"11",
         songName: `Lagdi Lahore Di <br><div class="subtitle">Street Dancer 3D</div>`,
-        poster: "img/11.jpg",
+        poster: "11.jpg",
     },
     {
         id:"12",
         songName: `Putt Jatt Da <br><div class="subtitle">Putt Jatt Da</div>`,
-        poster: "img/12.jpg",
+        poster: "12.jpg",
     },
     {
         id:"13",
         songName: `Baarishein <br><div class="subtitle">Atif Aslam</div>`,
-        poster: "img/13.jpg",
+        poster: "13.jpg",
     },
     {
         id:"14",
         songName: `Vaaste <br><div class="subtitle">Dhvani Bhanushali</div>`,
-        poster: "img/14.jpg",
+        poster: "14.jpg",
     },
     {
         id:"15",
         songName: `Lut Gaye <br><div class="subtitle">Jubin Nautiyal</div>`,
-        poster: "img/15.jpg",
+        poster: "15.jpg",
     },
 ]
 
